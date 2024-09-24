9d8220fefb3726fdfafcf55109d998f186adac89
Saroj Dahal <sarojdahal@Sarojs-Air.lan>
2024-09-25 02:13:10 +1000

    change the authidication

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 class profession(models.Model):
     name = models.CharField(max_length=50)
     profession=models.CharField(max_length=50)
+    
 
 def __str__(self):
         return self.name
@@ -14,6 +15,7 @@ class contact(models.Model):
     Name=models.CharField('Name', max_length=200)
     E_mail=models.EmailField('E-Mail', blank=False)
     phone_number = PhoneNumberField('Phone-Number',null=True, blank=True)
+    
 
     
     Description= models.TextField('Your message Request', max_length=200)
